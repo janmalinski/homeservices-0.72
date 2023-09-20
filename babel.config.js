8b968f82ce46c720211057be4778341fc95de42e
Jan Malinski <janmalinski@MacBook-Pro-Jan.local>
2023-09-20 19:42:24 +0200

    add babel.config.js, react-native.config.js, tsconfig.json

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        root: ['./'],
+        alias: {
+          '@src': './src',
+          '@components': './src/components',
+          '@designSystem': './src/components/designSystem',
+          '@utils': './src/utils',
+          '@helpers': 'src/helpers'
+        },
+      },
+    ],
+    "@babel/plugin-transform-export-namespace-from",
     'react-native-reanimated/plugin',
   ],
 };
